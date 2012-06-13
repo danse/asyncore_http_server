9951bc49de3395c5f38236d299b6682a1f1fba0a
Francesco Occhipinti <focchipinti@gmail.com>
2012-06-13 15:29:44 +0200

    Updated with the new merge req

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='asyncore_http_server',
-    version='0.4',
+    version='0.5',
     author='Josiah Carlson',
     packages=['asyncore_http_server']
     )
