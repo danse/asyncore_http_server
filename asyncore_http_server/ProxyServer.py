32d406d01c077ecb367f36d4d845ae5dfe7eb2d6
Francesco Occhipinti <focchipinti@gmail.com>
2012-09-21 10:00:27 +0200

    Big simplifications coming from the hypothesis that the "callback" will not raise weird exceptions

--- asyncore_http_server/ProxyServer.py
+++ asyncore_http_server/ProxyServer.py
@@ -1,18 +1,3 @@
-""" The <httpProxyServer> process data from an http client connection starting 
-from lower level.
-
-For each message received, in the 'process_data' the <httpProxyServer> builds a 
-dict with data inside and route it to the Receiver specified in conf: see
-Scheduler specified in the server section. 
-
-Details:
-'process_data' calls 'parse_message' that retrieves GET or POST contents and 
-returns a dict with the data inside.
-
-'process_data' calls 'send_msg_to_sender' that retrieves connection for routing
-and sends the dict using 'connection.process_message'. Connection is the class
-Receiver of the dict. 
-"""
 import cgi
 import socket
 import logging
@@ -39,7 +24,7 @@ REMOTE2LOCAL='<=='
 
 from asyncore_http_server import RequestHandler
 
-class HTTPCodes:
+class Codes:
     
     # successful
     OK_200 = 200
@@ -90,85 +75,13 @@ class HTTPCodes:
     GatewayTimeout_504          = 504
     HTTPVersionNotSupported_505 = 505
 
-# Logical errors during process
-
-ROUTER_ERROR = "Router Error" 
-PARSER_ERROR = 'Parser Error' 
-POLICY_ERROR = 'Policy Error' 
-MEMORY_ERROR = 'Memory Error' 
-MEMORY_DUPLICATE_ERROR = 'Duplicate entry' 
-SCHEDULER_ERROR = 'Scheduler Error' 
-SENDER_ERROR = 'Sender Error'
-CONTEXT_ERROR = 'Context Error'
-UNKNOWN_ERROR = 'Unknown Error'
-CACHING_ERROR = 'Caching Error'
-EXPIRED_ERROR = 'Expired Error'
-
-class NodeErrors(object):
-    """
-    A set of internal granpa codes, useful have an entry
-    node different from SMTP
-    """
-    # A generic error which can be retried
-    RETRY             = 'Retry Error'
-    ROUTER            = ROUTER_ERROR 
-    PARSER            = PARSER_ERROR 
-    POLICY            = POLICY_ERROR 
-    MEMORY            = MEMORY_ERROR 
-    MEMORY_DUPLICATE  = MEMORY_DUPLICATE_ERROR 
-    SCHEDULER         = SCHEDULER_ERROR
-    SENDER            = SENDER_ERROR
-    CONTEXT           = CONTEXT_ERROR
-    CACHING           = CACHING_ERROR
-    # A generic error which can not be retried
-    UNKNOWN           = UNKNOWN_ERROR
-
-
-class NodeError(Exception):
-    """ Exception class to manage processing error """
-
-    def __init__(self, args):
-        self.status = args[0] # Status code
-        self.error = args[1] # Text error string
-        self.flist = args[2] # List of (functor_name, (functor_args))
-        # ((f1,(f1_p1, f1_p2, ..)),(f2,(f2_p1, f2_p2, ..)))
-
-# Reply codes related to the logical errors during process
-PROCESS_ERRORS = {
-    NodeErrors.ROUTER : HTTPCodes.InternalServerError_500,
-    NodeErrors.PARSER : HTTPCodes.InternalServerError_500,
-    NodeErrors.POLICY : HTTPCodes.InternalServerError_500,
-    NodeErrors.MEMORY : HTTPCodes.InternalServerError_500,
-    NodeErrors.MEMORY_DUPLICATE : HTTPCodes.InternalServerError_500,
-    NodeErrors.SCHEDULER : HTTPCodes.InternalServerError_500,
-    NodeErrors.SENDER : HTTPCodes.InternalServerError_500,
-    NodeErrors.CACHING : HTTPCodes.InternalServerError_500,
-    NodeErrors.CONTEXT : HTTPCodes.InternalServerError_500,
-    NodeErrors.UNKNOWN : HTTPCodes.InternalServerError_500 }
-    
 class HTTPServer(RequestHandler):
                 
     def __init__(self, conn, addr, server, callback, logger=None):
         # Initialize the ProxyNode (Receiver)
         RequestHandler.__init__(self, conn, addr, server)
-        
-        self.raise_flist = ((self.reset_internal_data, ()), )
-        self.reset_internal_data()            
-
         self.mainlog = logger or logging
-
         self.callback = callback
-        
-    def raise_args(self, error):
-        pe = PROCESS_ERRORS[NodeErrors.UNKNOWN]
-        if PROCESS_ERRORS.has_key(error):
-            pe = PROCESS_ERRORS[error]
-        return pe, error, self.raise_flist
-        
-    def reset_internal_data(self):  
-        self._channel_msgid = 0
-        self._sender_args = {}
-        self._sender_msg = None
         
     def get_local_address(self):
         try:
@@ -213,64 +126,14 @@ class HTTPServer(RequestHandler):
             self.outgoing.append(None)
             return
         try:       
-            self.process_data()
-        except Exception as e:
-            self.mainlog.error(str(e))
-            return self.handle_error()
-
-    def handle_error(self):
-        nil, t, v, tbinfo = compact_traceback()
-        http_status_code = HTTPCodes.OK_200
-        error = tbinfo
-        flist = []
-        
-        if isinstance(v, NodeError):
-            http_status_code = v.status
-            error = v.error
-            flist = v.flist
-        
-        self.mainlog.error('[%s] %s %s' % (str('here'), error, str(v)))
-        
-        if not isinstance(v, socket.error):
-            self.push_final(http_status_code, 'text/plain', 'server error')
-        
-        for functor, args in flist:
-            # sometimes a user functor method will crash.
-            try:
-                functor(*args)
-            except Exception as e:
-                self.mainlog.error(str(e))
-                
-    ################################################
-    ###    Processing Data Callbacks             ###
-    ################################################
-    
-    def process_data(self, msg=""): 
-        """ Process data from client connection, starting from lower level """  
-        
-        try:
-            
             # Parse message and check errors
-            self._sender_msg, error = self.parse_message()
-            if error:
-                raise NodeError(self.raise_args(error))
-    
-            # Get message id and update the private data
-            if self._sender_msg.has_key('Message-ID'):
-                self._channel_msgid = self._sender_msg['Message-ID']       
-            
-            # Prepare args for sendere connection
-            self._sender_args = {'fileno' : self._fileno,}
-
+            request = self.parse_data()
             # Send message to the current sender and check errors
-            error = self.callback(self._sender_msg, self.process_reply)
-            if error:
-                raise NodeError(self.raise_args(error))
-            
+            self.callback(request, self.process_reply)
         except Exception as e:
-            self.mainlog.error(str(e))
-            return self.handle_error()
-            
+            self.mainlog.exception()
+            self.handle_error()
+
     ################################################
     ###    Processing Reply Callbacks            ###
     ################################################
@@ -281,6 +144,12 @@ class HTTPServer(RequestHandler):
         (status_code, content_type, message_body) = self.parse_args(args)
         self.push_final(status_code, content_type, message_body)       
 
+    def handle_error(self, code=None):
+        if not code:
+            code = Codes.InternalServerError_500
+        if not isinstance(v, socket.error):
+            self.push_final(code, 'text/plain', 'server error')
+        
     def push_final(self, status_code=200, ctype="text/html", entity_body=""):
         self.mainlog.info("Status-Code: %s, Content-Type %s, Entity-Body: %s" % \
             (status_code, ctype, entity_body)) 
@@ -293,7 +162,6 @@ class HTTPServer(RequestHandler):
         self.update_b(len(entity_body))
         self.outgoing.append(entity_body)
         self.outgoing.append(None)                    
-        self.reset_internal_data()
 
     ################################################
     ###    Utilities                             ###
@@ -303,42 +171,37 @@ class HTTPServer(RequestHandler):
         '''status_code, content_type, message_body'''
         return args[:3]
         
-    def parse_message(self, msg="", route=""):
+    def parse_data(self):
         """ Given a GET or a POST extract contents from it and returns a dict 
         containing all the data
         """
         
-        dataDict = {}
-        
-        try :   
-            dataDict['headers'] = None
-            dataDict['postdata'] = None
-            dataDict['params'] = None
-            dataDict['path'] = self.path
-            if self.command == 'GET':
-                try:
-                    dataDict['params'] = self.body
-                except Exception as e:
-                    self.mainlog.error(str(e))
-            elif self.command == "POST":
-                try:
-                    qspos = self.path.find('?')
-                    if qspos>=0:
-                        dataDict['params'] = cgi.parse_qs(self.path[qspos+1:], keep_blank_values=1)
-                        dataDict['path'] = self.path[:qspos]
-                    dataDict['headers'] = self.headers
-                    dataDict['postdata'] = self.body
-                except Exception as e:
-                    self.mainlog.error(str(e))
-            msg =  "%s - %s - %s " % (dataDict['headers'],
-                                      dataDict['postdata'],
-                                      dataDict['params'])
-            self.log_msg(msg, "==>")
-        except Exception as e:
-            self.mainlog.error(str(e))
-            return None, NodeErrors.PARSER
-        
-        return dataDict, None
+        request = {
+            'headers' : None,
+            'postdata' : None,
+            'params' : None,
+            'path' : self.path,
+            }
+        if self.command == 'GET':
+            try:
+                request['params'] = self.body
+            except Exception as e:
+                self.mainlog.error(str(e))
+        elif self.command == "POST":
+            try:
+                qspos = self.path.find('?')
+                if qspos>=0:
+                    request['params'] = cgi.parse_qs(self.path[qspos+1:], keep_blank_values=1)
+                    request['path'] = self.path[:qspos]
+                request['headers'] = self.headers
+                request['postdata'] = self.body
+            except Exception as e:
+                self.mainlog.error(str(e))
+        msg =  "%s - %s - %s " % (request['headers'],
+                                  request['postdata'],
+                                  request['params'])
+        self.log_msg(msg, "==>")
+        return request
 
     def do_POST(self):
         ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
